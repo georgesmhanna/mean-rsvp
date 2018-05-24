e6114fc1f6532402cd83c5f67ac9f536595c2366
George Mhanna <georgesmhanna92@gmail.com>
2018-05-24 11:07:49 +0300

    Node Basic Setup: Installing dependencies (cors, body parser, method override) Adding Configuration file + auth 0 config Adding route

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: 'georgesmhanna.auth0.com',
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://user:password@ds233320.mlab.com:33320/mean'
+};
